639f6c484666879ee8744387455f20c891a1ffe7
ruoshuisixue@sina.com <ruoshuisixue@sina.com>
2016-04-12 17:22:43 +0800

    增加动画

--- app/src/main/java/xyz/ruoxue/animator/MainActivity.java
+++ app/src/main/java/xyz/ruoxue/animator/MainActivity.java
@@ -1,15 +1,59 @@
 package xyz.ruoxue.animator;
 
+import android.animation.ObjectAnimator;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import xyz.ruoxue.animator.customeView.CircleView;
+
 public class MainActivity extends BaseActivity {
 
 
+    private CircleView cv_home;
+
     @Override
     protected void initView() {
+        cv_home = $(R.id.cv_home);
 
     }
 
     @Override
     protected int initLayout() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_rotation:
+                startRotation("rotation");
+                break;
+            case R.id.menu_rotationx:
+                startRotation("rotationX");
+                break;
+            case R.id.menu_rotationy:
+                startRotation("rotationY");
+                break;
+        }
+
+
+
+        return true;
+    }
+
+    private void startRotation(String rotation) {
+        ObjectAnimator objectAnimator=ObjectAnimator.ofFloat(cv_home, rotation, 0f, 360f);
+        objectAnimator.setDuration(1000);
+
+        objectAnimator.setRepeatCount(-1);
+        objectAnimator.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        return true;
     }
 }
--- app/src/main/java/xyz/ruoxue/animator/customeView/CircleView.java
+++ app/src/main/java/xyz/ruoxue/animator/customeView/CircleView.java
@@ -1,6 +1,10 @@
 package xyz.ruoxue.animator.customeView;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -37,13 +41,26 @@ import android.view.View;
 // +----------------------------------------------------------------------
 // | Author: 李上凡 ruoshuisixue@sina.com  创建时间: 2016/4/12_16:29  包名: xyz.ruoxue.animator.customeView
 // +----------------------------------------------------------------------
+
+
+/**
+ *
+ *
+ *
+ *
+ */
 public class CircleView extends View {
+    private Paint textPaint;//文字颜色
+    private Paint arcPaint;//外部弧形颜色
+    private  Paint innerPaint;
+
+
     public CircleView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CircleView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -51,12 +68,99 @@ public class CircleView extends View {
         init();
     }
 
+    /*
+    init
+
+     */
     private void init() {
+        textPaint = new Paint();
+        textPaint.setColor(Color.BLUE);
+        arcPaint = new Paint();
+        arcPaint.setColor(Color.CYAN);
+        innerPaint=new Paint();
+        innerPaint.setColor(Color.RED);
     }
 
+    /**
+     * <p>
+     * Measure the view and its content to determine the measured width and the
+     * measured height. This method is invoked by {@link #measure(int, int)} and
+     * should be overridden by subclasses to provide accurate and efficient
+     * measurement of their contents.
+     * </p>
+     * <p/>
+     * <p>
+     * <strong>CONTRACT:</strong> When overriding this method, you
+     * <em>must</em> call {@link #setMeasuredDimension(int, int)} to store the
+     * measured width and height of this view. Failure to do so will trigger an
+     * <code>IllegalStateException</code>, thrown by
+     * {@link #measure(int, int)}. Calling the superclass'
+     * {@link #onMeasure(int, int)} is a valid use.
+     * </p>
+     * <p/>
+     * <p>
+     * The base class implementation of measure defaults to the background size,
+     * unless a larger size is allowed by the MeasureSpec. Subclasses should
+     * override {@link #onMeasure(int, int)} to provide better measurements of
+     * their content.
+     * </p>
+     * <p/>
+     * <p>
+     * If this method is overridden, it is the subclass's responsibility to make
+     * sure the measured height and width are at least the view's minimum height
+     * and width ({@link #getSuggestedMinimumHeight()} and
+     * {@link #getSuggestedMinimumWidth()}).
+     * </p>
+     *
+     * @param widthMeasureSpec  horizontal space requirements as imposed by the parent.
+     *                          The requirements are encoded with
+     *                          {@link android.view.View.MeasureSpec}.
+     * @param heightMeasureSpec vertical space requirements as imposed by the parent.
+     *                          The requirements are encoded with
+     *                          {@link android.view.View.MeasureSpec}.
+     * @see #getMeasuredWidth()
+     * @see #getMeasuredHeight()
+     * @see #setMeasuredDimension(int, int)
+     * @see #getSuggestedMinimumHeight()
+     * @see #getSuggestedMinimumWidth()
+     * @see android.view.View.MeasureSpec#getMode(int)
+     * @see android.view.View.MeasureSpec#getSize(int)
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
 
+    }
 
+    /**
+     * Implement this to do your drawing.
+     *
+     * @param canvas the canvas on which the background will be drawn
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
 
+        canvas.drawText("test", getWidth() / 2, getHeight() / 2, textPaint);
 
+        RectF oval = new RectF();
+        oval.left = 0;
+        oval.right = getWidth();
+        oval.top = (getHeight() - getWidth()) / 2;
+        oval.bottom = getWidth();
+//        canvas.drawRect(oval,arcPaint);
+        canvas.drawArc(oval, 0, 360, true, arcPaint);
+        //--------------------------------------------------内部圆-------------------------------------------------------------------------//
+
+        RectF inneroval = new RectF();
+
+        inneroval.left = getWidth()/4;
+        inneroval.right = getWidth()*3 / 4;
+        inneroval.top = (getHeight() - getWidth()) / 2+getWidth()/4;
+        inneroval.bottom = (getHeight() - getWidth()) / 2+getWidth()/2;
+
+//        canvas.drawRect(inneroval,innerPaint);
+        canvas.drawArc(inneroval, 0, 360, true, innerPaint);
+    }
 }
